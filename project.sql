54d31e502fbbfe94504127df515132a100bcfff3
Patrick McGrath <mcgrath.p@husky.neu.edu>
2016-06-07 18:24:26 -0400

    Msg

--- project.sql
+++ project.sql
@@ -27,3 +27,53 @@ CREATE TABLE colleges
 (
 	college_name       VARCHAR(30)    PRIMARY KEY
 );
+
+CREATE TABLE notes
+(
+	student_from_id	VARCHAR(30) NOT NULL,
+    student_to_id 	VARCHAR(30),
+    note_id 		INT			PRIMARY KEY 	UNIQUE,
+    note_text		MEDIUMTEXT,
+    CONSTRAINT student_from_fk
+		FOREIGN KEY (student_from_id)
+		REFERENCES students (student_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT student_to_fk
+		FOREIGN KEY (student_to_id)
+		REFERENCES students (student_id)
+		ON DELETE SET NULL ON UPDATE SET NULL
+);
+
+CREATE table groups
+(
+	group_name			VARCHAR(50) NOT NULL,
+    group_id			INT			PRIMARY KEY,
+    college_name		VARCHAR(30),
+    purpose_statement	TEXT,
+    CONSTRAINT college_name_groups_fk
+		FOREIGN KEY (college_name)
+		REFERENCES colleges (college_name)
+		ON UPDATE SET NULL ON DELETE CASCADE
+);
+
+CREATE TABLE groups_to_students
+(
+	group_id 		INT				NOT NULL,
+    student_id		VARCHAR(30)		NOT NULL,
+    PRIMARY KEY (group_id, student_id),
+    CONSTRAINT group_id_reference_fk
+		FOREIGN KEY (group_id)
+        REFERENCES groups (group_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT student_id_reference_fk
+		FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+    
+
+    
+    
+    
+    
+	
